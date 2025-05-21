cb354541723185881d18a0dff0e8bd57330531e3
niketh-coder <niketh2025@qesecurities.com>
2025-05-21 10:03:50 +0530

    Update chat_workflow.py for openai

--- chat_workflow.py
+++ chat_workflow.py
@@ -2,16 +2,27 @@ import os
 import requests
 from groq import Groq
 from chat_state import ChatState
+from dotenv import load_dotenv
 from langgraph.graph import StateGraph, END
+from langchain_openai import ChatOpenAI
 from langgraph.checkpoint.memory import InMemorySaver
 from utils import merge_context_chunks, search, iterative_context_expansion, recursive_qa, ask_fn
 
+load_dotenv()
 MAX_RECENT_MESSAGES = 3
 memory = InMemorySaver()
 GROQ_API_KEY = os.getenv("GROQ_API_KEY")
+OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
+ANTHROPIC_API_KEY = os.getenv("ANTHROPIC_API_KEY")
 GROQ_MODEL = "llama-3.3-70b-versatile"
 client = Groq(api_key = GROQ_API_KEY)
 used_chunks = set()
+llm = ChatOpenAI(
+    model="gpt-4o",
+    temperature=0.2,
+    max_tokens=1000,
+    # openai_api_key=API_KEY,
+)
 
 def get_used_chunks():
     return used_chunks
@@ -38,30 +49,34 @@ def create_workflow():
                 f"{message_text}"
             )
     
-            response = requests.post(
-                "https://api.groq.com/openai/v1/chat/completions",
-                headers={"Authorization": f"Bearer {GROQ_API_KEY}"},
-                json={
-                    "model": GROQ_MODEL,
-                    "messages": [
-                        {
-                            "role": "system",
-                            "content": (
-                                "You are a conversation summarizer. Condense the following conversation into a brief summary "
-                                "that captures the key points discussed and questions asked. Focus on the essential information "
-                                "that would be needed for context in future conversation."
-                            )
-                        },
-                        {
-                            "role": "user",
-                            "content": prompt
-                        }
-                    ]
-                }
-            ).json()
+            # response = requests.post(
+            #     "https://api.groq.com/openai/v1/chat/completions",
+            #     headers={"Authorization": f"Bearer {GROQ_API_KEY}"},
+            #     json={
+            #         "model": GROQ_MODEL,
+            #         "messages": [
+            #             {
+            #                 "role": "system",
+            #                 "content": (
+            #                     "You are a conversation summarizer. Condense the following conversation into a brief summary "
+            #                     "that captures the key points discussed and questions asked. Focus on the essential information "
+            #                     "that would be needed for context in future conversation."
+            #                 )
+            #             },
+            #             {
+            #                 "role": "user",
+            #                 "content": prompt
+            #             }
+            #         ]
+            #     }
+            # ).json()
+
+            # TODO: check here once
+            response = llm.invoke(message_text)
     
-            history_summary = response["choices"][0]["message"]["content"]
-            messages = recent_messages  # discard old messages
+            # history_summary = response["choices"][0]["message"]["content"]
+            history_summary = response.content
+            messages = recent_messages  
     
        
         final_history = history_summary + "\n\n" + "\n\n".join(
@@ -72,7 +87,7 @@ def create_workflow():
         state['history'] = final_history
         return state
 
-    def preprocess_with_llama(state): 
+    def preprocess_with_openai(state): 
         messages = [
             {
                 "role": "system",
@@ -92,17 +107,20 @@ def create_workflow():
         ]
         
         try:
-            response = requests.post(
-                "https://api.groq.com/openai/v1/chat/completions",
-                headers={"Authorization": f"Bearer {GROQ_API_KEY}"},
-                json={
-                    "model": GROQ_MODEL,
-                    "messages": messages,
-                    "temperature": 0.3,
-                    "max_tokens": 1000
-                }
-            )
-            response_data = response.json()
+            # response = requests.post(
+            #     "https://api.groq.com/openai/v1/chat/completions",
+            #     headers={"Authorization": f"Bearer {GROQ_API_KEY}"},
+            #     json={
+            #         "model": GROQ_MODEL,
+            #         "messages": messages,
+            #         "temperature": 0.3,
+            #         "max_tokens": 1000
+            #     }
+            # )
+            # response_data = response.json()
+
+            response = llm.invoke(messages)
+            response_data = response.content
 
             if "choices" in response_data and len(response_data["choices"]) > 0:
                 enhanced_query = response_data["choices"][0]['message']["content"].strip()
@@ -123,7 +141,7 @@ def create_workflow():
         used_chunks.clear()
         query = state['processed_question']
         results = search(query , top_k=5)
-        results = merge_context_chunks(results , complete_cnt = 2)
+        results = merge_context_chunks(results , complete_cnt = 10)
         state['context_chunks'] = results
         print('chunks for context retrieved')
         return state
@@ -133,7 +151,7 @@ def create_workflow():
         client = Groq(api_key = GROQ_API_KEY)
         query = state['processed_question']
         context_chunks = state['context_chunks']
-        context = iterative_context_expansion(query ,  context_chunks ,  max_rounds=1)
+        context = iterative_context_expansion(query ,  context_chunks ,  max_rounds=3)
         state['context'] = context
         print('final context ceated')
         return state
@@ -154,7 +172,7 @@ def create_workflow():
         print('answer generated')
         return state
 
-    builder.add_node("preprocess", preprocess_with_llama)
+    builder.add_node("preprocess", preprocess_with_openai)
     builder.add_node("retrieve", retrieve)
     builder.add_node("iterative_query_search", iterative_query_search)
     builder.add_node("manage_conversation_history", manage_conversation_history)
